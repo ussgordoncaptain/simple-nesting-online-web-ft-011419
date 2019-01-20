a03acdcff411f447191f8a409ea1fb3fe0c11f50
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-20 07:54:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,7 @@ def adding_matz
      :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }
-
-    
+  return programmer_hash  
 end
 
 def changing_alan
@@ -107,8 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for]= "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
      
 end
 
@@ -129,5 +128,5 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-
+  return programmer_hash
 end
